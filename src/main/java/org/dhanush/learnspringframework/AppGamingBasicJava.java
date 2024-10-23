77e758211d4ce5091e797438bd7b6054c19092e7
dhanushgr <grdhanush8@gmail.com>
2024-10-23 19:22:23 +0530

    Created PacmanGame it is loosely coupled

--- src/main/java/org/dhanush/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/org/dhanush/learnspringframework/AppGamingBasicJava.java
@@ -2,12 +2,14 @@ package org.dhanush.learnspringframework;
 
 import org.dhanush.learnspringframework.game.GameRunner;
 import org.dhanush.learnspringframework.game.MarioGame;
+import org.dhanush.learnspringframework.game.PacManGame;
 import org.dhanush.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) {
-        var game = new MarioGame();
+        //var game = new MarioGame();
         //var game = new SuperContraGame();
+        var game = new PacManGame();
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
