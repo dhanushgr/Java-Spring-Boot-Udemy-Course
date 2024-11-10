9a2770fa6a8f156a3cd26aa4047cb9d34f2d62f0
dhanushgr <grdhanush8@gmail.com>
2024-11-10 23:00:34 +0530

    updated test refactored the whole code learned about different assert methods learned about different annotation uses like beforeAll, beforeEach, afterAll, afterEach

--- src/test/java/org/dhanush/Junit/MyMathTest.java
+++ src/test/java/org/dhanush/Junit/MyMathTest.java
@@ -2,18 +2,50 @@ package org.dhanush.Junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class MyMathTest {
 
+    private MyMath math = new MyMath();
+
     @Test
-    void test(){
-        int[] numbers = {1,2,3};
-        MyMath math = new MyMath();
-        int result = math.CalculateSum(numbers);
-        System.out.println(result);
-        int expectedResult = 6;
-        assertEquals(expectedResult, result);
+    void calculateSum_ThreeMemberArray(){
+        assertEquals(6, math.CalculateSum(new int[]{1,2,3}));
+    }
+
+    @Test
+    void testWithNoElements(){
+        assertEquals(0, math.CalculateSum(new int[]{}));
+    }
+
+    //This test has nothing do with the MyMath class
+    //Its the different assert methods present
+    @Test
+    void differentTest(){
+        assertArrayEquals(new int[] {1,2}, new int[] {1,2});
+    }
+
+    /*
+        Before each test this will appear
+     */
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("After Each");
+    }
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After All");
     }
 
 }
